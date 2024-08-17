d8f708df35d3bb9d0390408288b39ab4262938b9
Asl1yev <asliyevahmadjon321@gmail.com>
2024-08-17 17:26:01 +0200

    Uzum_market_cole
    uzum_market_clone

---
+++ Uzum_Market/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import SignupView,LoginView,LogoutView,add_to_cart,view_cart
+
+
+
+urlpatterns=[
+    path('signup/',SignupView,name='register'),
+    path('login/',LoginView,name='login'),
+    path('logout/',LogoutView,name='logout'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', view_cart, name='view_cart'),
+
+]
